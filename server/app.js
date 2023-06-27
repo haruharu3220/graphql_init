9c68ecec2491855105f403bd0e61f89559454f66
Hayato <hayato.314.nico@gmail.com>
2023-06-28 07:34:34 +0900

    graphqlHTTPのインストールを{}で囲って実施 {}を除けるとサーバが立ち上がらなかった

--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
 const express = require("express");
-const graphqlHTTP = require("express-graphql");
+const { graphqlHTTP } = require('express-graphql');
+
+const mongoose = require('mongoose');
 const app = express();
 
+//mongoDBの接続
+mongoose.connect("mongodb+srv://admin:Admin12345@cluster0.aejcr4p.mongodb.net/")
+mongoose.connection.once('open',()=>{
+    console.log("db connected");
+})
 app.use('/graphql',graphqlHTTP({
-    
+
 }))
 app.listen(4000,() => {
     console.log("listening on")
